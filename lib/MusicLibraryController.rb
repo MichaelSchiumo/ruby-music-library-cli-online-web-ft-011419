343a69f135af25485776d594f3dc917c73929d6a
Michael J. Schiumo <mschiumo18@gmail.com>
2019-02-04 20:29:51 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -24,15 +24,6 @@ class MusicLibraryController
     input = gets.strip
   end  
   
-  
-  
-  
-  
-
-  
-  
-  
-  
   def list_songs 
     Song.all.sort {|a, b| a.name <=> b.name}.each_with_index {|song, i| puts "#{i + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
   end  
